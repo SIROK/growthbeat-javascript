f8ed4c71833fe5c714e83aca22a8e05335a127c5
sangotaro <sango0715@gmail.com>
2015-07-03 11:59:09 +0900

    Add GrowthbeatHttpClient Class

---
+++ source/growthbeat-core/ts/http/growthbeat-http-client.ts
@@ -0,0 +1,60 @@
+import nanoajax = require('nanoajax');
+
+interface AjaxParams {
+    method:string,
+    url:string,
+    body?:string
+}
+
+class GrowthbeatHttpClient {
+    constructor(private baseUrl:string, private timeout:number = 0) {
+
+    }
+
+    get(api:string, params:any, success:Function, error:Function) {
+        return this.request('GET', api, params, success, error);
+    }
+
+    post(api:string, params:any, success:Function, error:Function) {
+        return this.request('POST', api, params, success, error);
+    }
+
+    put(api:string, params:any, success:Function, error:Function) {
+        return this.request('PUT', api, params, success, error);
+    }
+
+    delete(api:string, params:any, success:Function, error:Function) {
+        return this.request('DELETE', api, params, success, error);
+    }
+
+    request(method:string, api:string, params:any, success:Function, error:Function) {
+        let body = Object.keys(params).map((key)=> {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+        }).join('&');
+
+        let ajaxParams = <AjaxParams>{
+            method
+        };
+
+        if (method === 'GET') {
+            ajaxParams.url = `${this.baseUrl}${api}?${body}`;
+        } else {
+            ajaxParams.url = `${this.baseUrl}${api}`;
+            ajaxParams.body = body;
+        }
+
+        // TODO: handle timeout
+
+        nanoajax.ajax(ajaxParams, (code:number, responseText:string)=> {
+            if (code === 200) {
+                let data = JSON.parse(responseText);
+                success(data, code);
+            } else {
+                let err = {};
+                error(err, code);
+            }
+        });
+    }
+}
+
+export = GrowthbeatHttpClient;
