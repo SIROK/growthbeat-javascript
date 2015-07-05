c25893e3de0496e9fe04e0b0ed23e3c05fb9e44e
sangotaro <sango0715@gmail.com>
2015-07-05 17:55:23 +0900

    Modify component-emitter.d.ts

--- local_typings/microevent.d.ts
+++
@@ -1,9 +0,0 @@
-declare class MicroEvent {
-    bind(event:string, listener:Function):any;
-    unbind(event:string, listener:Function):any;
-    trigger(event:string, ...args: any[]);
-}
-
-declare module 'microevent' {
-    export = MicroEvent;
-}
