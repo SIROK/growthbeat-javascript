aa0a1763700969caa346048cf9214711448bb297
sangotaro <sango0715@gmail.com>
2015-07-06 10:47:57 +0900

    Update

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,6 @@
 ///<reference path='../local_typings/nanoajax.d.ts' />
 ///<reference path='../local_typings/component-emitter.d.ts' />
+///<reference path='../local_typings/t.d.ts' />
 
 import Growthbeat = require('./growthbeat/index');
 import GrowthbeatCore = require('./growthbeat-core/index');
