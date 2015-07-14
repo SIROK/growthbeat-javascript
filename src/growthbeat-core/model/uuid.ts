f90d68dd0fd22d52c67aee509de9d981bbfb880c
Ogawa Shigeru <ogawa@sirok.co.jp>
2015-07-15 00:04:04 +0900

    create uuid model

--- src/growthbeat-core/model/uuid.ts
+++ src/growthbeat-core/model/uuid.ts
@@ -6,58 +6,56 @@ var HTTP_CLIENT_TIMEOUT = 60 * 1000;
 
 var httpClient = new GrowthbeatHttpClient(HTTP_CLIENT_BASE_URL, HTTP_CLIENT_TIMEOUT);
 
-class Client extends Emitter {
-    private id:string;
+class Uuid extends Emitter {
+    private uuid:string;
 
     constructor(data?:any) {
         super();
+        this.uuid = data.uuid;
     }
 
     static load():Client {
         if (!window.localStorage) {
             return null;
         }
-        var clientData = window.localStorage.getItem('growthbeat:client');
+        var uuidData = window.localStorage.getItem('growthbeat:uuid');
         if (clientData == null) {
             return null;
         }
-        return new Client(JSON.parse(clientData));
+        return new Uuid(JSON.parse(uuidData));
     }
 
-    static save(data:Client) {
+    static save(data:Uuid) {
         if (!data || !window.localStorage) {
             return;
         }
-        window.localStorage.setItem('growthbeat:client', JSON.stringify(data));
+        window.localStorage.setItem('growthbeat:uuid', JSON.stringify(data));
     }
 
     static create(applicationId:string, credentialId:string):Client {
         var opt = {
-            params: {
-                applicationId,
-                credentialId
-            },
+            params: {},
             dataType: 'jsonp'
         };
 
-        var client = new Client();
+        var uuid = new Uuid();
 
-        httpClient.get('1/clients/create', opt,
+        httpClient.get('1/uuid/create', opt,
             (data, code) => {
                 console.log(data, code);
-                client.emit('created');
+                uuid.emit('created');
             },
             (err, code) => {
                 client.emit('error');
             });
 
-        return client;
+        return uuid;
     }
 
-    getId():string {
-        return this.id;
+    getUuid():string {
+        return this.uuid;
     }
 
 }
 
-export = Client;
+export = Uuid;
